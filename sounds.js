1b5630ba5cddf8a77ffa8acdf29c091a38bf4fde
zzo38 <108554049+zzo38@users.noreply.github.com>
2021-04-01 04:00:57 +0000

    Change the sounds.js to use the sound names instead of numbers (although this does not find all of the sounds; also, it may be reverted if needed)

--- sounds.js
+++ sounds.js
@@ -6,6 +6,58 @@ const fs=require("fs");
 const out=new hamarc.File(1);
 const hdr=Buffer.allocUnsafe(12);
 const fil=fs.openSync(process.argv[2],"r");
+const names=`
+ SPLASH
+ POUR
+ DOOR
+ GLASS
+ BANG
+ UNHH
+ UH_OH
+ FROG
+ THWIT
+ KLINKK
+ POWER
+ KLECK
+ CLICK
+ SMALL_POP
+ DINK
+ TICK
+ CHYEW
+ CHEEP
+ ANHH
+ BRRRT
+ BRRREEET
+ BWEEP
+ DRLRLRINK
+ FFFFTT
+ WAHOO
+ YEEHAW
+ OLDPHONE
+ RATTLE
+ BEEDEEP
+ THMP_thmp
+ BEDOINGNG
+ HEARTBEAT
+ LOCK
+ TAHTASHH
+ BOOOM
+ VACUUM
+ RATCHET2
+ DYUPE
+ UNCORK
+ BOUNCE
+ JAYAYAYNG
+ DEEP_POP
+ RATCHET1
+ GLISSANT
+ BUZZER
+ FAROUT
+ KEWEL
+ WHACK
+ STEAM
+ HAWK
+`.split("\n").map(x=>x.trim()).filter(x=>x);
 let n=0;
 let o=process.argv[3]?parseInt(process.argv[3],16):0x5CD92;
 for(;;) {
@@ -14,7 +66,7 @@ for(;;) {
   let len=hdr.readUInt32LE(4);
   let buf=Buffer.allocUnsafeSlow(len+8);
   fs.readSync(fil,buf,0,len,o);
-  out.put(n+".WAV",buf);
+  out.put(names[n]+".WAV",buf);
   o+=len+8;
   n++;
 }
